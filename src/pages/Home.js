371c4a4d3a2d18f76c9c26443a875ce9e8a2b711
pgm-stijwalr <147693656+pgm-stijwalr@users.noreply.github.com>
2024-12-20 14:58:11 +0100

    Removed ; at animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ export default function Home() {
         return (
             <div className="flex justify-center items-center h-screen w-screen">
                 <div className="w-40 h-40">
-                    <AnimatedLogo className="w-full" />;
+                    <AnimatedLogo className="w-full" />
                 </div>
             </div>
         );

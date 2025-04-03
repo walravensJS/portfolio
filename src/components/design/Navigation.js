f1528b2febea79861aa4b2871777df604670f45a
pgm-stijwalr <stijwalr@student.arteveldehs.be>
2025-04-03 14:15:39 +0200

    changed cases to projects

--- src/components/design/Navigation.js
+++ src/components/design/Navigation.js
@@ -139,7 +139,7 @@ export default function Navigation() {
                                     }
                                     onClick={toggleModal}
                                 >
-                                    CASES
+                                    PROJECTS
                                 </NavLink>
                             </li>
                             <li>
